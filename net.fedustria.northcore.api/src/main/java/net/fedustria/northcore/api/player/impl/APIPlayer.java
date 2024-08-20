41c6537af78eb8b3fbab40cfcfafbf6ed54f3aa7
Florian <mrminecrafttv19@gmail.com>
2024-08-20 03:31:52 +0200

    feat: added test values to the apiplayer

--- net.fedustria.northcore.api/src/main/java/net/fedustria/northcore/api/player/impl/APIPlayer.java
+++ net.fedustria.northcore.api/src/main/java/net/fedustria/northcore/api/player/impl/APIPlayer.java
@@ -1,4 +1,17 @@
 package net.fedustria.northcore.api.player.impl;
 
+import lombok.Getter;
+import lombok.NonNull;
+
 public class APIPlayer {
+
+    @Getter
+    public String language = "german";
+
+    public String replacePlaceholder(@NonNull String message) {
+        return message
+                .replaceAll("%prefix%", "cooler prefix")
+                ;
+    }
+
 }
